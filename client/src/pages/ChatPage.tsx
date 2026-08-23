b1d8b3b180069f29d92ea329de901526721c1d8a
Aryan Dahiya <aryan23dahiya@gmail.com>
2026-08-23 19:19:11 +0530

    feat(ui): P2-FE-04 reactive layout and P2-FE-07 CSS nesting cleanup

--- client/src/pages/ChatPage.tsx
+++ client/src/pages/ChatPage.tsx
@@ -3,7 +3,6 @@ import DesktopNavigation from "../components/Navigation/DesktopNavigation";
 import MobileNavigation from "../components/Navigation/MobileNavigation";
 import Users from "../components/Users/Users";
 import Chats from "../components/Chats/Chats";
-import Header from "../components/Header/Header";
 import LoadingIndicator from "../components/UI/LoadingIndicator/LoadingIndicator";
 import GroupChatWidget from "../components/Widgets/GroupChatWidget";
 import ChatDeleteModal from "../components/UI/ChatDeleteModal";
@@ -11,44 +10,36 @@ import ImageWidget from "../components/Widgets/ImageWidget";
 import { ThemeContext } from "../contexts/ThemeContext";
 
 const ChatPage = () => {
+    const { groupChatWidget, logoutLoading, deleteModal, imageWidget } = useContext(ThemeContext);
 
-    const isMobileScreen = () => window.innerWidth <= 647;
-
-    const { groupChatWidget } = useContext(ThemeContext);
-    const { logoutLoading } = useContext(ThemeContext);
-    const { deleteModal } = useContext(ThemeContext);
-    const { imageWidget } = useContext(ThemeContext);
+    const isDimmed = Boolean(groupChatWidget || logoutLoading || deleteModal);
 
     return (
         <>
             {groupChatWidget && <GroupChatWidget />}
-
             {deleteModal && <ChatDeleteModal />}
-
             {imageWidget && <ImageWidget />}
 
-            {logoutLoading &&
+            {logoutLoading && (
                 <div className="fixed top-[40%] left-[45%] md:top-[40%] md:left-[50%] z-50">
                     <LoadingIndicator />
                 </div>
-            }
-
-            {isMobileScreen() ? (
-                <>
-                    <Header message="Hii" />
-                    <Chats />
-                </>
-            ) : (
-                <div className={`md:flex md:flex-row ${(groupChatWidget || logoutLoading || deleteModal) && "opacity-70"}`}>
-                    <DesktopNavigation />
-                    <MobileNavigation />
-                    <Users />
-                    <Chats />
-                </div>
             )}
+
+            {/* Desktop / Tablet side-by-side view */}
+            <div className={`hidden md:flex md:flex-row ${isDimmed ? "opacity-70" : ""}`}>
+                <DesktopNavigation />
+                <MobileNavigation />
+                <Users />
+                <Chats />
+            </div>
+
+            {/* Mobile full-screen chat view */}
+            <div className={`flex flex-col md:hidden w-full h-[100dvh] ${isDimmed ? "opacity-70" : ""}`}>
+                <Chats />
+            </div>
         </>
     );
-
-}
+};
 
 export default ChatPage;
